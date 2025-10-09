ceec8bf4ccdd79b1ccf271412ea67ca4d4425341
Gennadii Zakharov <gz3@sanger.ac.uk>
2025-10-09 12:52:45 +0100

    Added two tests for corrupted  VCFs.

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -4,7 +4,20 @@ use std::process::Command;
 #[test]
 fn valid_vcf() {
     // Path to a known test VCF in the repository
-    let test_vcf = "test_vcf/HG001_GRCh38_1_22_v4.2.1_benchmark.vcf.gz";
+    let test_vcf = "test_vcf/HG001_head.vcf";
+
+    Command::cargo_bin("validvcf")
+        .expect("binary should build")
+        .arg("--quiet")
+        .arg(test_vcf)
+        .assert()
+        .success();
+}
+
+#[test]
+fn valid_vcf_bgz() {
+    // Path to a known test VCF in the repository
+    let test_vcf = "test_vcf/HG001_head.vcf.bgz";
 
     Command::cargo_bin("validvcf")
         .expect("binary should build")
@@ -26,3 +39,30 @@ fn non_exist_vcf() {
         .assert()
         .failure();
 }
+
+#[test]
+fn missed_info() {
+    // Path to a known test VCF in the repository
+    let test_vcf = "test_vcf/HG001_missed_info.vcf";
+
+    Command::cargo_bin("validvcf")
+        .expect("binary should build")
+        .arg("--quiet")
+        .arg(test_vcf)
+        .assert()
+        .failure();
+}
+
+#[test]
+fn corrupted_format() {
+    // Path to a known test VCF in the repository
+    let test_vcf = "test_vcf/HG001_corrupted_format.vcf";
+
+    Command::cargo_bin("validvcf")
+        .expect("binary should build")
+        .arg("--quiet")
+        .arg(test_vcf)
+        .assert()
+        .failure();
+}
+
